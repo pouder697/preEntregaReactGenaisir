fc5018ef477b9f2136e10765710647d0c903b05c
Andres Genaisir <andres.genaisir@gmail.com>
2024-07-05 22:00:07 -0400

    agregado de css modular y estilizado de ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ItemList from '../ItemList/ItemList';
+import '../ItemListContainer/itemlistcontainerstyles.css';
 
 function ItemListContainer() {
     return (
-        <ul>
-            <ItemList producto='Hoodie' precio='40.000' />
+        <ul className="listContainer">
+            <ItemList className='listContainer' producto='Hoodie' precio='40.000' />
             <ItemList producto='Pantalon' precio='20.000' />
             <ItemList producto='Remera' precio='15.000' />
         </ul>
